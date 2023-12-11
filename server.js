49e4a277b46567148ca695c59a3e2be1194c552e
MOGILEESWAR <47683272+mogili1402@users.noreply.github.com>
2023-12-11 15:36:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const port = 3001
 app.get('/', (req, res) => {
   res.sendFile(dir+"/index.html")
 })
+
+app.get('/callback', (req, res) => {
+  cosole.log(req.body,req.params)
+ res.send("ok")
+})
 app.get('/privacy-policy', (req, res) => {
   res.sendFile(dir+"/pri.html")
 })
