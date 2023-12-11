76623a6ffaefb41f952a390176f15355acadff0f
Mogileeswar <mogileshwar@way2news.co>
2023-12-11 15:52:16 +0530

    ds

---
+++ mo.js
@@ -0,0 +1,29 @@
+const { MongoClient } = require('mongodb');
+// or as an es module:
+// import { MongoClient } from 'mongodb'
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = 'Way2News_Ads';
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db(dbName);
+  const l= await db.collections()
+  console.log(l)
+  const collection = db.collection('documents');
+
+  // the following code examples can be pasted here...
+
+  return 'done.';
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
